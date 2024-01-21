03e2eccab7173f9745898f5fa74d23f533c41ed2
hariharn12 <140273143+hariharn12@users.noreply.github.com>
2024-01-21 21:04:11 +0530

    creating multiple box component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,14 @@ import { AddBoxComponent } from './add-box/add-box.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,RouterLink,AddBoxComponent,RouterLinkActive],
+  imports: [CommonModule, RouterOutlet, RouterLink, AddBoxComponent, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angluar-practice';
+  boxes: number[] = []
+  addbox() {
+    this.boxes.push(1)
+  }
 }
