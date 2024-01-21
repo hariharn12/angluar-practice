ef0aa72bbf7912b393060cf603eb4a454091138a
hariharn12 <140273143+hariharn12@users.noreply.github.com>
2024-01-21 21:09:40 +0530

    adding style for routerlinkactive state

--- src/app/client-list/client-list.component.ts
+++ src/app/client-list/client-list.component.ts
@@ -1,11 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-client-list',
   standalone: true,
-  imports: [CommonModule,RouterLink,RouterOutlet],
+  imports: [CommonModule,RouterLink,RouterOutlet,RouterLinkActive],
   templateUrl: './client-list.component.html',
   styleUrl: './client-list.component.css'
 })
